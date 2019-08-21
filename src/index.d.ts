e313513d8f9819a3ee28ff9a86c8ce614f39562b
Ondřej Bárta <email@ondrejbarta.xyz>
2019-08-21 22:21:48 +0200

    Updates

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,11 +1,17 @@
 export declare class ScrollNavigation {
     root: HTMLElement | HTMLBodyElement;
     children: Element[];
+    dotsRoot: HTMLElement;
+    dotsChildren: HTMLElement[];
+    currentIndex: number;
     constructor(root: HTMLElement);
     refreshChildren(): void;
+    refreshDots(): void;
+    setDotIndex(index: number): void;
     private initOnScrollEndListener;
     private initOnWindowSizeChangeListener;
     private initOnScrollListener;
     private snapToSlide;
     private getElementInTheMiddle;
+    private getCurrentIndex;
 }
